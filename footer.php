176e963f360b2bb291622fc3c62db6e77344d5e4
amansuryawanshi <52121580+amansuryawanshi@users.noreply.github.com>
2021-05-14 23:13:10 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,19 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
 
-        footer{
-            width: 100%;
-        }
-    </style>
-</head>
 <body>
-    <footer class="footer  bg-dark mt-5 ">
+    <footer class="footer  bg-dark mt-5 " style="width:100%">
 <div class="container">
 
 <p class="text-center text-white p-4">Designed and Developed with <i class="fas fa-heart" style="color: red;"></i> By Aman Suryawanshi</p>
